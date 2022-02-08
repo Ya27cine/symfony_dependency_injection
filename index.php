4329b1b399f33531f3688503dc0ea1201f4d6675
KHELIFA Yassine <prostam27@gmail.com>
2022-02-08 22:06:39 +0100

    use méthode register()

--- index.php
+++ index.php
@@ -13,31 +13,39 @@ require __DIR__ . '/vendor/autoload.php';
 $container = new ContainerBuilder();
 
 //$controller = new OrderController($database, $mailer, $texter);
-$controllerDef = new Definition(OrderController::class, [
-    new Reference('database'),
-    new Reference('mailer.gmail'),
-    new Reference('texter.sms')
-]);
-$controllerDef->addMethodCall('sayHello');
-$container->setDefinition('oreder_controller', $controllerDef);
+// $controllerDef = new Definition(OrderController::class, [
+//     new Reference('database'),
+//     new Reference('mailer.gmail'),
+//     new Reference('texter.sms')
+// ]);
+// $container->setDefinition('oreder_controller', $controllerDef);
+// $controllerDef->addMethodCall('sayHello');
 
-
-
+$container->register('oreder_controller', OrderController::class)
+   ->setArguments([ new Reference('database'), 
+                    new Reference('mailer.gmail'), 
+                    new Reference('texter.sms')])
+    ->addMethodCall('sayHello');
 
 //$database = new Database();
-$databaseDef = new Definition(Database::class);
-$container->setDefinition('database', $databaseDef);
+// $databaseDef = new Definition(Database::class);
+// $container->setDefinition('database', $databaseDef);
 // $database = $container->get('database');
+$container->register('database', Database::class);
 
 //$texter = new SmsTexter("service.sms.com", "apikey123");
-$texterDef = new Definition(SmsTexter::class,[ "service.sms.com", "apikey123"]);
-$container->setDefinition('texter.sms', $texterDef);
+// $texterDef = new Definition(SmsTexter::class,[ "service.sms.com", "apikey123"]);
+// $container->setDefinition('texter.sms', $texterDef);
 // $texter = $container->get('texter.sms');
+$container->register('texter.sms', SmsTexter::class)
+->setArguments([ "service.sms.com", "apikey123"]);
 
 //$mailer = new GmailMailer("lior@gmail.com", "123456");
-$mailerDef = new Definition(GmailMailer::class, [ "lior@gmail.com", "123456"]);
-$container->setDefinition('mailer.gmail', $mailerDef);
+// $mailerDef = new Definition(GmailMailer::class, [ "lior@gmail.com", "123456"]);
+// $container->setDefinition('mailer.gmail', $mailerDef);
 // $mailer = $container->get('mailer.gmail');
+$container->register('mailer.gmail', GmailMailer::class)
+->setArguments([ "lior@gmail.com", "123456"]);
 
 
 $controller = $container->get('oreder_controller');
