febd084421527e58d78dcd6f66194240217099e7
KHELIFA Yassine <prostam27@gmail.com>
2022-02-28 22:18:42 +0100

    Cumuler Autowiring et Arguments spécifiques

--- index.php
+++ index.php
@@ -19,6 +19,24 @@ $container = new ContainerBuilder();
 $container->setParameter('mailer.gmail_user', "lior@gmail.com");
 $container->setParameter('mailer.gmail_password', "123456");
 
+$container->autowire('oreder_controller', OrderController::class)
+   ->setPublic(true);
+
+$container->autowire('database', Database::class);
+
+$container->autowire('mailer.smtp', SmtpMailer::class)
+->setArguments(['smtp://localhost', 'root', '123']);
+
+$container->register('texter.fax', FaxTexter::class)  // register <=> autowire + setAutowired 
+->setAutowired(true);
+
+$container->autowire('texter.sms', SmsTexter::class)
+->setArguments([ "service.sms.com", "apikey123"]);
+
+$container->autowire('mailer.gmail', GmailMailer::class)
+->setArguments([ "%mailer.gmail_user%", "%mailer.gmail_password%"]);
+
+
 $container->setAlias(OrderController::class, 'oreder_controller')->setPublic(true);
 $container->setAlias(Database::class, 'database');
 
@@ -33,50 +51,6 @@ $container->setAlias(FaxTexter::class, 'texter.fax');
 // dynamic 
 $container->setAlias(TexterInterface::class, 'texter.sms');
 
-
-
-//$controller = new OrderController($database, $mailer, $texter);
-// $controllerDef = new Definition(OrderController::class, [
-//     new Reference('database'),
-//     new Reference('mailer.gmail'),
-//     new Reference('texter.sms')
-// ]);
-// $container->setDefinition('oreder_controller', $controllerDef);
-// $controllerDef->addMethodCall('sayHello');
-
-$container->register('oreder_controller', OrderController::class)
-   ->setPublic(true)
-   ->setAutowired(true)
-//    ->setArguments([ new Reference(Database::class), 
-//                     new Reference(GmailMailer::class), 
-//                     new Reference(SmsTexter::class)])
-    ->addMethodCall('sayHello');
-
-//$database = new Database();
-// $databaseDef = new Definition(Database::class);
-// $container->setDefinition('database', $databaseDef);
-// $database = $container->get('database');
-$container->register('database', Database::class);
-
-$container->register('mailer.smtp', SmtpMailer::class)
-->setArguments(['smtp://localhost', 'root', '123']);
-
-$container->register('texter.fax', FaxTexter::class);
-
-
-//$texter = new SmsTexter("service.sms.com", "apikey123");
-// $texterDef = new Definition(SmsTexter::class,[ "service.sms.com", "apikey123"]);
-// $container->setDefinition('texter.sms', $texterDef);
-// $texter = $container->get('texter.sms');
-$container->register('texter.sms', SmsTexter::class)
-->setArguments([ "service.sms.com", "apikey123"]);
-
-//$mailer = new GmailMailer("lior@gmail.com", "123456");
-// $mailerDef = new Definition(GmailMailer::class, [ "lior@gmail.com", "123456"]);
-// $container->setDefinition('mailer.gmail', $mailerDef);
-// $mailer = $container->get('mailer.gmail');
-$container->register('mailer.gmail', GmailMailer::class)
-->setArguments([ "%mailer.gmail_user%", "%mailer.gmail_password%"]);
 
 $container->compile();
 
--- src/Database/Database.php
+++ src/Database/Database.php
@@ -6,6 +6,7 @@ use App\Model\Order;
 
 class Database
 {
+
     public function insertOrder(Order $order)
     {
         var_dump("REQUETE DATABASE POUR INSERER LA COMMANDE");
